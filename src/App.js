55d10acc9837df8b12ded5d7574e87e514945fa9
Datfatcat <55812671+Datfatcat@users.noreply.github.com>
2021-11-14 20:51:06 -0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,36 +1,21 @@
-import axios from "axios";
-import React from "react";
+import React, { Component } from "react";
 import './App.css';
+import GetBlock from "./components/GetBlock";
 
-const client = axios.create({
-  baseURL: "http://ec2-54-151-16-73.us-west-1.compute.amazonaws.com:8888/block/height/0",
-  withCredentials: false,
-  headers: {'Access-Control-Allow-Headers' : 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
-            'Access-Control-Allow-Methods' : 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-            'Access-Control-Allow-Credentials' : true,
-            'Access-Control-Allow-Origin': '*'}
-});
+class App extends React.Component {
 
-export default function App() {
-  const [post, setPost] = React.useState(null);
+		state = {
+      visible: true
+		};
 
-  React.useEffect(() => {
-    client.get('/0').then((response) => {
-      setPost(response.data);
-    });
-  }, []);
+	render() {
+    return(
+      <div className="App">
+        <GetBlock />
+      </div>
+    )
+  }
 
-  if (!post) return null;
-
-  return (
-    <div>
-      <h1>{post.hash}</h1>
-      <h1>{post.height}</h1>
-      <h1>{post.body}</h1>
-      <h1>{post.time}</h1>
-      <h1>{post.previousBlockHash}</h1>
-    </div>
-  );
 }
 
-//export default App;
+export default App;
---
+++ src/components/GetBlock.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+export default class GetBlock extends React.Component {
+
+	state = {
+		loading: true,
+		user: null
+	};
+
+	// ComponentDidMount is used to
+	// execute the code
+	async componentDidMount() {
+		const response = await fetch("http://104.34.230.121:3000/block/height/0", { mode: 'cors' });
+    const data = await response.json();
+    this.setState({user: data, loading: false});
+	}
+
+	render() {
+    if(this.state.loading){
+      return <div>Loading...</div>;
+    }
+
+    if(!this.state.user){
+      return <div>No user</div>;
+    }
+
+    return(
+      <div>
+        <div>Hash : {this.state.user.hash}</div>
+        <div>Height : {this.state.user.height}</div>
+        <div>Body : {this.state.user.body}</div>
+        <div>Time : {this.state.user.time}</div>
+        <div>Previous Block Hash : {this.state.user.previousBlockHash}</div>
+      </div>
+    );
+  }
+
+}
+
