675b1042411c7d983ade613313ac18c26e3ae91d
Datfatcat <55812671+Datfatcat@users.noreply.github.com>
2021-11-22 23:07:06 -0600

    Add comments toGetBlock.js

--- src/components/GetBlock.js
+++ src/components/GetBlock.js
@@ -17,8 +17,11 @@ export default class GetBlock extends React.Component {
 	// ComponentDidMount is used to
 	// execute the code
 	async componentDidMount() {
+		// fetch block transactions from the following link
 		const response = await fetch("https://cors-everywhere.herokuapp.com/http://104.34.230.121:3000/block/heights", { mode: 'cors' });
+	//take the response from the API
     const data = await response.json();
+	// set the response as data
     this.setState({user: data, loading: false});
     console.log(response);
     console.log(data);
@@ -60,16 +63,25 @@ export default class GetBlock extends React.Component {
         <div>
           <h1>Reputation Score Full Blockchain</h1>
           &nbsp;
+	      // map function allows iteration of the whole block based on a unique ID
           {this.state.user.map(block => (
             <div key={block.uuid}>
+	      // FOR EACH BLOCK
+	      	// print out the hash, body, time, and previous block hash
+	      
+	      	// Redundant to add subheading for block heigt, so it's used to indicate which block instead
               <h4>Block {block.height}</h4>
               <h6 align="left">Hash</h6>
                 <div align="left">{block.hash}</div>
                 <br/>
               <h6 align="left">Body:</h6>
+			// decode body from HEX to TEXT
+			// https://www.npmjs.com/package/hex-encode-decode
                 <div align="left">{decode(block.body)}</div>
                 <br/>
               <h6 align="left">Time:</h6>
+		// Time format is HH:MM:SS
+		// https://stackoverflow.com/questions/1322732/convert-seconds-to-hh-mm-ss-with-javascript
                 <div align="left">{new Date(block.time * 1000).toISOString().substr(11, 8)}</div>
                 <br/>
               <h6 align="left">Previous Block Hash:</h6>
